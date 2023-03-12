7d9782c1974a429cbc2121b3491e7d3215367fcb
Kinshuk Banerjee <kinshuk2002.king@gmail.com>
2023-03-12 10:42:46 +0000

    Add files via upload

---
+++ Lab/Test.js
@@ -0,0 +1,30 @@
+var x,y,z;  // Global Variable
+function PopUpBox(){
+    var msg;    // Local Variable
+    msg = "This is IWT Lecture"
+    alert(msg)
+}
+function add(){
+    x = 14;
+    y = 36;
+    z = x + y;
+    alert("Sum = "+z);
+}
+function sub(){
+    x = 14;
+    y = 36;
+    z = x - y;
+    alert("difference = "+z);
+}
+function mul(){
+    x = 14;
+    y = 36;
+    z = x * y;
+    alert("product = "+z);
+}
+function div(){
+    x = 14;
+    y = 36;
+    z = x / y;
+    alert("division = "+z);
+}
